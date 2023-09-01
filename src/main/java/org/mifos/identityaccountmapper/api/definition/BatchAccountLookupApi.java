fd2711608cb480e4a3a5add4523cebc31a5ae774
Ankita Ranjan <ankita@fynarfin.io>
2023-09-01 13:31:01 +0530

    Api Compliance- Fixing Bulk Account Lookup API

--- src/main/java/org/mifos/identityaccountmapper/api/definition/BatchAccountLookupApi.java
+++ src/main/java/org/mifos/identityaccountmapper/api/definition/BatchAccountLookupApi.java
@@ -1,5 +1,6 @@
 package org.mifos.identityaccountmapper.api.definition;
 
+import org.mifos.identityaccountmapper.data.BatchAccountLookupRequestDTO;
 import org.mifos.identityaccountmapper.data.RequestDTO;
 import org.mifos.identityaccountmapper.data.ResponseDTO;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.ExecutionException;
 
 public interface BatchAccountLookupApi {
-    @PostMapping("/accountLookup")
-    ResponseEntity<ResponseDTO> batchAccountLookup(@RequestHeader(value="X-CallbackURL") String callbackURL, @RequestBody RequestDTO requestBody, @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId) throws ExecutionException, InterruptedException;
+    @PutMapping("/beneficiary")
+    ResponseEntity<ResponseDTO> batchAccountLookup(@RequestHeader(value="X-CallbackURL") String callbackURL,
+                                                   @RequestBody BatchAccountLookupRequestDTO requestBody,
+                                                   @RequestHeader(value = "X-Registering-Institution-ID") String registeringInstitutionId) throws ExecutionException, InterruptedException;
 }
