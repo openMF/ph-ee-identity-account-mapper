a3b38f0e9a8c7d7c41eddc9df06ff4f5f50f88b4
logoutdhaval <dhaval@fynarfin.io>
2024-02-29 18:32:33 +0530

    ttl increace

--- src/main/java/org/mifos/identityaccountmapper/api/implementation/AccountLookupCallbackController.java
+++ src/main/java/org/mifos/identityaccountmapper/api/implementation/AccountLookupCallbackController.java
@@ -66,7 +66,7 @@ public class AccountLookupCallbackController implements AccountLookupCallback {
 
         // if (zeebeClient != null) {
 
-        zeebeClient.newPublishMessageCommand().messageName(ACCOUNT_LOOKUP).correlationKey(transactionId).timeToLive(Duration.ofMillis(60000))
+        zeebeClient.newPublishMessageCommand().messageName(ACCOUNT_LOOKUP).correlationKey(transactionId).timeToLive(Duration.ofMillis(360000))
                 .variables(variables).send().join();
         logger.info("-------------> variable published");
         // }
